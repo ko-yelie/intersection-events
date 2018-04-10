dbf7e061d7100b7c593ae984400cf7917e13a6af
Ko.Yelie <ko.yelie@gmail.com>
2018-04-10 22:00:37 +0900

    Use banner-package

--- bili.config.js
+++ bili.config.js
@@ -1,20 +1,4 @@
-const pkg = require('./package.json')
-
-const dependencies = pkg.dependencies
-  ? ` *
- * Dependencies
-${Object.keys(pkg.dependencies)
-      .map(name => ` * https://www.npmjs.com/package/${name}\n`)
-      .join('')}`
-  : ''
-
-const banner = `/*!
- * ${pkg.name} v${pkg.version}
- * ${pkg.homepage}
- *
- * (c) ${pkg.year}-present ${pkg.author}
- * Released under the ${pkg.license} License.
-${dependencies} */`
+const banner = require('banner-package')
 
 module.exports = {
   format: ['es', 'iife', 'iife-min'],
